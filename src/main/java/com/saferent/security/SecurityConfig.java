3596f3fa8c8570a20970a76e2b7b111f3553fd24
Altug Heper <altugheper@gmail.com>
2023-02-21 21:45:29 +0100

    21/02/23finalversion

--- src/main/java/com/saferent/security/SecurityConfig.java
+++ src/main/java/com/saferent/security/SecurityConfig.java
@@ -1,4 +1,74 @@
 package com.saferent.security;
 
+import com.saferent.security.jwt.AuthTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    //!!! AMACIM: Encoder, Provider, AuthTokenFilter gibi yapilari olusturmak
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and().
+                authorizeRequests().
+                antMatchers("/login",
+                        "/register",
+                        "/",
+                        "/index.html").permitAll().
+                anyRequest().authenticated();
+
+        //!!! AuthTokenFilter yazdiktan sonra addFilter yazilacak
+
+        return http.build();
+
+    }
+
+    // !!! Encoder
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+
+    //!!! Provider
+    @Bean
+    public DaoAuthenticationProvider authProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    //!!! AuthenticationManager
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class).
+                authenticationProvider(authProvider()).
+                build();
+    }
+
+    //!!! AuthTokenFilter (JWT token ureten ve valide eden class)
+    @Bean
+    public AuthTokenFilter authTokenFilter(){
+        return new AuthTokenFilter();
+    }
 }
--- src/main/java/com/saferent/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/saferent/security/service/UserDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package com.saferent.security.service;
 
+import com.saferent.domain.User;
+import com.saferent.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userService.getUserByEmail(email);
+        return UserDetailsImpl.build(user);
+    }
 }
