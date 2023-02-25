4fbc1632f41dd80ed76c8e372e3bc42113ad01ed
Altug Heper <altugheper@gmail.com>
2023-02-25 21:56:06 +0100

    25/02/23lastV

--- src/main/java/com/saferent/controller/ImageFileController.java
+++ src/main/java/com/saferent/controller/ImageFileController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/files")
 public class ImageFileController {
 
-    private final ImageFileService
+    private final ImageFileService imageFileService;
 
+    public ImageFileController(ImageFileService imageFileService) {
+        this.imageFileService = imageFileService;
+    }
 }
--- src/main/java/com/saferent/domain/ImageFile.java
+++ src/main/java/com/saferent/domain/ImageFile.java
@@ -4,10 +4,38 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
+@Table(name = "t_imagefile")
 public class ImageFile {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id; // security icin Long yerine String
+
+    private String name;
+
+    private String type;
+
+    private long length;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private ImageData imageData;
+
+    public ImageFile(String name, String type, ImageData imageData, long length){ // length bilgisini ImageData'daki data variable'indan aliniyor
+        this.name=name;
+        this.type=type;
+        this.imageData=imageData;
+        this.length=imageData.getData().length;
+    }
+
 }
