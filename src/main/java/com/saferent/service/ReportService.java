8eb4a325179e70560f1c427ea2c3a5822aa54497
Altug Heper <altugheper@gmail.com>
2023-03-05 19:05:54 +0100

    05/03/23v4

--- src/main/java/com/saferent/service/ReportService.java
+++ src/main/java/com/saferent/service/ReportService.java
@@ -1,6 +1,15 @@
 package com.saferent.service;
 
+import com.saferent.domain.Car;
+import com.saferent.domain.Reservation;
+import com.saferent.domain.User;
+import com.saferent.exception.message.ErrorMessage;
+import com.saferent.report.ExcelReporter;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.List;
 
 @Service
 public class ReportService {
@@ -12,5 +21,37 @@ public class ReportService {
         this.userService = userService;
         this.carService = carService;
         this.reservationService = reservationService;
+    }
+
+    public ByteArrayInputStream getUserReport() {
+
+        List<User> users = userService.getUsers();
+
+        try {
+            return ExcelReporter.getUserExcelReport(users);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
+        }
+    }
+
+    public ByteArrayInputStream getCarReport() {
+        List<Car> cars = carService.getAllCar();
+
+        try {
+            return ExcelReporter.getCarExcelReport(cars);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
+        }
+
+    }
+
+    public ByteArrayInputStream getReservationReport() {
+        List<Reservation> reservations = reservationService.getAll();
+
+        try {
+            return ExcelReporter.getReservationExcelReport(reservations);
+        } catch (IOException e) {
+            throw new RuntimeException(ErrorMessage.EXCEL_REPORT_ERROR_MESSAGE);
+        }
     }
 }
